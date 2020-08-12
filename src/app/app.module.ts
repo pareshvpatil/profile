5dbe185d01fbe73ba26df3cc67f422b74ca6f39f
pareshvpatil <paresh.patil1305@gmail.com>
2020-08-12 20:02:12 +0530

    Added: resume page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {SafePipe} from "./common/SafePipe";
 import { ResumeComponent } from './components/resume/resume.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ResumeComponent } from './components/resume/resume.component';
     ProjectsComponent,
     ContactComponent,
     SafePipe,
-    ResumeComponent
+    ResumeComponent,
+    FooterComponent
   ],
     imports: [
         BrowserModule,
--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare type SocialLinks = 'github' | 'linkedin' | 'twitter' | 'gmail' | 'whatsapp';
+
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -12,4 +14,23 @@ export class ResumeComponent implements OnInit {
   ngOnInit() {
   }
 
+  navigateTo(location: SocialLinks) {
+    switch (location) {
+      case "github":
+        window.open("https://www.github.com/pareshvpatil")
+        break;
+      case "linkedin":
+        window.open("https://www.linkedin.com/in/paresh-v-patil")
+        break;
+      case "twitter":
+        window.open("https://www.twitter.com/deftlad")
+        break;
+      case "gmail":
+        window.open("mailto:com.deftlad@gmail.com")
+        break;
+      case "whatsapp":
+        window.open("https://wa.me/918087497099")
+        break;
+    }
+  }
 }
